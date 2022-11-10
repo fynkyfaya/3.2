8cd30d3a7b74cc5c75d4ebf52aea6d3cbfaae3bf
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-11-10 20:27:35 +0200

    Add files via upload

---
+++ Program (3).cs
@@ -0,0 +1,12 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+for (var number = 1; number <= n; number++) {
+    var sum = 0;
+    for (var x = 1; x <= number / 2; x++) {
+        if (number % x == 0) sum += x;
+    }
+    
+    if (number == sum) {
+        Console.WriteLine(number);
+    }
+}
